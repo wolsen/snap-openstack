4d24c54e4bca43c7a7ef15346839e6fa911c0aee
Chi Wai Chan <chiwai.chan@canonical.com>
2023-08-16 17:02:06 +0800

    Add missing offers for cos plugin based on the upstream COS repository [1].
    [1] https://github.com/canonical/cos-lite-bundle/tree/main

--- sunbeam-python/sunbeam/plugins/cos/etc/deploy-cos/outputs.tf
+++ sunbeam-python/sunbeam/plugins/cos/etc/deploy-cos/outputs.tf
@@ -20,6 +20,11 @@ output "prometheus-metrics-offer-url" {
   value       = juju_offer.prometheus-metrics-offer.url
 }
 
+output "prometheus-receive-remote-write-offer-url" {
+  description = "URL of the prometheus receive remote write endpoint offer"
+  value       = juju_offer.prometheus-receive-remote-write-offer.url
+}
+
 output "loki-logging-offer-url" {
   description = "URL of the loki logging offer"
   value       = juju_offer.loki-logging-offer.url
@@ -28,4 +33,9 @@ output "loki-logging-offer-url" {
 output "grafana-dashboard-offer-url" {
   description = "URL of the grafana dashboard offer"
   value       = juju_offer.grafana-dashboard-offer.url
+}
+
+output "alertmanager-karma-dashboard-offer-url" {
+  description = "URL of the alertmanager karma dashboard endpoint offer"
+  value       = juju_offer.alertmanager-karma-dashboard-offer.url
 }
