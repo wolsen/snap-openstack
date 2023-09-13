8231ca15b1e59f42a74d892f62d04b2968de53c4
Hemanth Nakkina <hemanth.nakkina@canonical.com>
2023-09-13 09:01:00 +0530

    Pin juju terraform provider to 0.8.0
    sunbeam-terraform fails with new versions of
    juju terraform provider, see [1]. Pin the
    version to 0.8.0 until [1] is fixed and released.
    
    [1] juju/terraform-provider-juju#310

--- cloud/etc/deploy-microceph/main.tf
+++ cloud/etc/deploy-microceph/main.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     juju = {
       source  = "juju/juju"
-      version = ">= 0.7.0"
+      version = "= 0.8.0"
     }
   }
 
--- sunbeam-python/sunbeam/plugins/cos/etc/deploy-cos/main.tf
+++ sunbeam-python/sunbeam/plugins/cos/etc/deploy-cos/main.tf
@@ -20,7 +20,7 @@ terraform {
   required_providers {
     juju = {
       source  = "juju/juju"
-      version = ">= 0.7.0"
+      version = "= 0.8.0"
     }
   }
 }
