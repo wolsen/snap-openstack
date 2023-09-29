0e237e7bb9c4abf97c98170c74a27ea75ca818ec
Liam Young <liam.young@canonical.com>
2023-09-29 11:09:57 +0000

    Implement LDAP plugin

---
+++ sunbeam-python/sunbeam/plugins/ldap/plugin.py
@@ -0,0 +1,421 @@
+# Copyright (c) 2023 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+from pathlib import Path
+from typing import Optional
+
+import click
+import yaml
+from packaging.version import Version
+from rich.console import Console
+from rich.status import Status
+
+from sunbeam.clusterd.client import Client
+from sunbeam.clusterd.service import (
+    ClusterServiceUnavailableException,
+    ConfigItemNotFoundException,
+)
+from sunbeam.commands.juju import JujuStepHelper
+from sunbeam.commands.openstack import OPENSTACK_MODEL
+from sunbeam.commands.terraform import (
+    TerraformException,
+    TerraformHelper,
+    TerraformInitStep,
+)
+from sunbeam.jobs.common import (
+    BaseStep,
+    Result,
+    ResultType,
+    read_config,
+    run_plan,
+    update_config,
+)
+from sunbeam.jobs.juju import JujuHelper, JujuWaitException, TimeoutException, run_sync
+from sunbeam.plugins.interface.v1.openstack import (
+    OpenStackControlPlanePlugin,
+    TerraformPlanLocation,
+)
+
+LOG = logging.getLogger(__name__)
+console = Console()
+
+APPLICATION_DEPLOY_TIMEOUT = 900  # 15 minutes
+
+
+class DisableLDAPDomainStep(BaseStep, JujuStepHelper):
+    """Generic step to enable OpenStack application using Terraform"""
+
+    def __init__(
+        self,
+        tfhelper: TerraformHelper,
+        jhelper: JujuHelper,
+        plugin: OpenStackControlPlanePlugin,
+        domain_name: str,
+    ) -> None:
+        """Constructor for the generic plan.
+
+        :param tfhelper: Terraform helper pointing to terraform plan
+        :param jhelper: Juju helper with loaded juju credentials
+        :param plugin: Plugin that uses this plan to perform callbacks to
+                       plugin.
+        """
+        super().__init__(
+            f"Enable OpenStack {plugin.name}",
+            f"Enabling OpenStack {plugin.name} application",
+        )
+        self.tfhelper = tfhelper
+        self.jhelper = jhelper
+        self.plugin = plugin
+        self.model = OPENSTACK_MODEL
+        self.domain_name = domain_name
+        self.client = Client()
+
+    def run(self, status: Optional[Status] = None) -> Result:
+        """Apply terraform configuration to deploy openstack application"""
+        config_key = self.plugin.get_tfvar_config_key()
+
+        try:
+            tfvars = read_config(self.client, config_key)
+        except ConfigItemNotFoundException:
+            tfvars = {}
+        tfvars.update(self.plugin.set_tfvars_on_enable())
+        if tfvars.get("ldap_apps") and self.domain_name in tfvars["ldap_apps"]:
+            del tfvars["ldap_apps"][self.domain_name]
+        else:
+            return Result(ResultType.FAILED, "Domain not found")
+        self.tfhelper.write_tfvars(tfvars)
+        update_config(self.client, config_key, tfvars)
+
+        try:
+            self.tfhelper.apply()
+        except TerraformException as e:
+            return Result(ResultType.FAILED, str(e))
+
+        return Result(ResultType.COMPLETED)
+
+
+class UpdateLDAPDomainStep(BaseStep, JujuStepHelper):
+    def __init__(
+        self,
+        tfhelper: TerraformHelper,
+        jhelper: JujuHelper,
+        plugin: OpenStackControlPlanePlugin,
+        charm_config: str,
+    ) -> None:
+        """Constructor for the generic plan.
+
+        :param tfhelper: Terraform helper pointing to terraform plan
+        :param jhelper: Juju helper with loaded juju credentials
+        :param plugin: Plugin that uses this plan to perform callbacks to
+                       plugin.
+        """
+        super().__init__(
+            f"Enable OpenStack {plugin.name}",
+            f"Enabling OpenStack {plugin.name} application",
+        )
+        self.tfhelper = tfhelper
+        self.jhelper = jhelper
+        self.plugin = plugin
+        self.model = OPENSTACK_MODEL
+        self.charm_config = charm_config
+        self.client = Client()
+
+    def run(self, status: Optional[Status] = None) -> Result:
+        """Apply terraform configuration to deploy openstack application"""
+        config_key = self.plugin.get_tfvar_config_key()
+
+        try:
+            tfvars = read_config(self.client, config_key)
+        except ConfigItemNotFoundException:
+            tfvars = {}
+        if tfvars["ldap_apps"].get(self.charm_config["domain-name"]):
+            tfvars["ldap_apps"][self.charm_config["domain-name"]] = self.charm_config
+        else:
+            return Result(ResultType.FAILED, "Domain not found")
+
+        self.tfhelper.write_tfvars(tfvars)
+        update_config(self.client, config_key, tfvars)
+
+        try:
+            self.tfhelper.apply()
+        except TerraformException as e:
+            return Result(ResultType.FAILED, str(e))
+        charm_name = "keystone-ldap-{}".format(self.charm_config["domain-name"])
+        apps = ["keystone", charm_name]
+        LOG.debug(f"Application monitored for readiness: {apps}")
+        try:
+            run_sync(
+                self.jhelper.wait_until_active(
+                    self.model,
+                    apps,
+                    timeout=APPLICATION_DEPLOY_TIMEOUT,
+                )
+            )
+        except (JujuWaitException, TimeoutException) as e:
+            LOG.warning(str(e))
+            return Result(ResultType.FAILED, str(e))
+        return Result(ResultType.COMPLETED)
+
+
+class AddLDAPDomainStep(BaseStep, JujuStepHelper):
+    """Generic step to enable OpenStack application using Terraform"""
+
+    def __init__(
+        self,
+        tfhelper: TerraformHelper,
+        jhelper: JujuHelper,
+        plugin: OpenStackControlPlanePlugin,
+        charm_config: str,
+    ) -> None:
+        """Constructor for the generic plan.
+
+        :param tfhelper: Terraform helper pointing to terraform plan
+        :param jhelper: Juju helper with loaded juju credentials
+        :param plugin: Plugin that uses this plan to perform callbacks to
+                       plugin.
+        """
+        super().__init__(
+            f"Enable OpenStack {plugin.name}",
+            f"Enabling OpenStack {plugin.name} application",
+        )
+        self.tfhelper = tfhelper
+        self.jhelper = jhelper
+        self.plugin = plugin
+        self.model = OPENSTACK_MODEL
+        self.charm_config = charm_config
+        self.client = Client()
+
+    def run(self, status: Optional[Status] = None) -> Result:
+        """Apply terraform configuration to deploy openstack application"""
+        config_key = self.plugin.get_tfvar_config_key()
+
+        try:
+            tfvars = read_config(self.client, config_key)
+        except ConfigItemNotFoundException:
+            tfvars = {}
+        tfvars.update(self.plugin.set_tfvars_on_enable())
+        if tfvars.get("ldap_apps"):
+            tfvars["ldap_apps"][self.charm_config["domain-name"]] = self.charm_config
+        else:
+            tfvars["ldap_apps"] = {self.charm_config["domain-name"]: self.charm_config}
+        self.tfhelper.write_tfvars(tfvars)
+        update_config(self.client, config_key, tfvars)
+
+        try:
+            self.tfhelper.apply()
+        except TerraformException as e:
+            return Result(ResultType.FAILED, str(e))
+        charm_name = "keystone-ldap-{}".format(self.charm_config["domain-name"])
+        apps = ["keystone", charm_name]
+        LOG.debug(f"Application monitored for readiness: {apps}")
+        try:
+            run_sync(
+                self.jhelper.wait_until_active(
+                    self.model,
+                    apps,
+                    timeout=APPLICATION_DEPLOY_TIMEOUT,
+                )
+            )
+        except (JujuWaitException, TimeoutException) as e:
+            LOG.warning(str(e))
+            return Result(ResultType.FAILED, str(e))
+
+        return Result(ResultType.COMPLETED)
+
+
+class LDAPPlugin(OpenStackControlPlanePlugin):
+    version = Version("0.0.1")
+
+    def __init__(self) -> None:
+        super().__init__(
+            name="ldap",
+            tf_plan_location=TerraformPlanLocation.SUNBEAM_TERRAFORM_REPO,
+        )
+        self.config_flags = None
+
+    def set_tfvars_on_enable(self) -> dict:
+        """Set terraform variables to enable the application."""
+        return {
+            "ldap-channel": "2023.1/edge",
+            "enable-ldap": True,
+        }
+
+    def set_tfvars_on_disable(self) -> dict:
+        """Set terraform variables to disable the application."""
+        return {}
+
+    def set_tfvars_on_resize(self) -> dict:
+        """Set terraform variables to resize the application."""
+        return {}
+
+    def set_application_names(self) -> list:
+        """Application names handled by the terraform plan."""
+        return []
+
+    @click.command()
+    def enable_plugin(self):
+        """Enable ldap service."""
+        super().enable_plugin()
+
+    @click.command()
+    def disable_plugin(self) -> None:
+        """Disable OpenStack LDAP application."""
+        super().disable_plugin()
+
+    @click.command()
+    def list_domains(self) -> None:
+        """List LDAP backed domains."""
+        self.client = Client()
+        try:
+            tfvars = read_config(self.client, self.get_tfvar_config_key())
+        except ConfigItemNotFoundException:
+            tfvars = {}
+        click.echo(" ".join(tfvars.get("ldap_apps", {}).keys()))
+
+    @click.command()
+    @click.argument("domain-name")
+    @click.option(
+        "--domain-config-file",
+        required=True,
+        help="""
+        Config file with entries
+        """,
+    )
+    @click.option(
+        "--ca-cert-file",
+        required=False,
+        help="""
+        CA for contacting ldap
+        """,
+    )
+    def add_domain(
+        self, ca_cert_file: str, domain_config_file: str, domain_name: str
+    ) -> None:
+        """Add LDAP backed domain."""
+        with Path(domain_config_file).open(mode="r") as f:
+            content = yaml.safe_load(f)
+        charm_config = {
+            "ldap-config-flags": json.dumps(content),
+            "domain-name": domain_name,
+            "tls-ca-ldap": "",
+        }
+        data_location = self.snap.paths.user_data
+        tfhelper = TerraformHelper(
+            path=self.snap.paths.user_common / "etc" / f"deploy-{self.tfplan}",
+            plan=self._get_plan_name(),
+            backend="http",
+            data_location=data_location,
+        )
+        jhelper = JujuHelper(data_location)
+        plan = [
+            TerraformInitStep(tfhelper),
+            AddLDAPDomainStep(tfhelper, jhelper, self, charm_config),
+        ]
+
+        run_plan(plan, console)
+        click.echo(f"{domain_name} added.")
+
+    @click.command()
+    @click.argument("domain-name")
+    @click.option(
+        "--domain-config-file",
+        required=False,
+        help="""
+        Config file with entries
+        """,
+    )
+    @click.option(
+        "--ca-cert-file",
+        required=False,
+        help="""
+        CA for contacting ldap
+        """,
+    )
+    def update_domain(
+        self, ca_cert_file: str, domain_config_file: str, domain_name: str
+    ) -> None:
+        """Add LDAP backed domain."""
+        with Path(domain_config_file).open(mode="r") as f:
+            content = yaml.safe_load(f)
+        charm_config = {
+            "ldap-config-flags": json.dumps(content),
+            "domain-name": domain_name,
+            "tls-ca-ldap": "",
+        }
+        data_location = self.snap.paths.user_data
+        tfhelper = TerraformHelper(
+            path=self.snap.paths.user_common / "etc" / f"deploy-{self.tfplan}",
+            plan=self._get_plan_name(),
+            backend="http",
+            data_location=data_location,
+        )
+        jhelper = JujuHelper(data_location)
+        plan = [
+            TerraformInitStep(tfhelper),
+            UpdateLDAPDomainStep(tfhelper, jhelper, self, charm_config),
+        ]
+
+        run_plan(plan, console)
+
+    @click.command()
+    @click.argument("domain-name")
+    def remove_domain(self, domain_name: str) -> None:
+        """Remove LDAP backed domain."""
+        data_location = self.snap.paths.user_data
+        tfhelper = TerraformHelper(
+            path=self.snap.paths.user_common / "etc" / f"deploy-{self.tfplan}",
+            plan=self._get_plan_name(),
+            backend="http",
+            data_location=data_location,
+        )
+        jhelper = JujuHelper(data_location)
+        plan = [
+            TerraformInitStep(tfhelper),
+            DisableLDAPDomainStep(tfhelper, jhelper, self, domain_name),
+        ]
+        run_plan(plan, console)
+        click.echo(f"{domain_name} removed.")
+
+    @click.group()
+    def ldap_groups(self):
+        """Manage ldap."""
+
+    def commands(self) -> dict:
+        """Dict of clickgroup along with commands."""
+        commands = super().commands()
+        try:
+            enabled = self.enabled
+        except ClusterServiceUnavailableException:
+            LOG.debug(
+                "Failed to query for plugin status, is cloud bootstrapped ?",
+                exc_info=True,
+            )
+            enabled = False
+
+        if enabled:
+            commands.update(
+                {
+                    "init": [{"name": "ldap", "command": self.ldap_groups}],
+                    "ldap": [
+                        {"name": "list-domains", "command": self.list_domains},
+                        {"name": "add-domain", "command": self.add_domain},
+                        {"name": "update-domain", "command": self.update_domain},
+                        {"name": "remove-domain", "command": self.remove_domain},
+                    ],
+                }
+            )
+        return commands
---
+++ sunbeam-python/tests/unit/sunbeam/plugins/test_ldap.py
@@ -0,0 +1,335 @@
+# Copyright 2023 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+from pathlib import Path
+from unittest.mock import AsyncMock, Mock, patch
+
+import pytest
+
+from sunbeam.commands.terraform import TerraformException
+from sunbeam.jobs.common import ResultType
+from sunbeam.jobs.juju import TimeoutException
+from sunbeam.plugins.ldap.plugin import (
+    AddLDAPDomainStep,
+    DisableLDAPDomainStep,
+    LDAPPlugin,
+    UpdateLDAPDomainStep,
+)
+
+
+@pytest.fixture()
+def cclient():
+    with patch("sunbeam.plugins.ldap.plugin.Client") as p:
+        yield p
+
+
+@pytest.fixture()
+def ccclient():
+    with patch("sunbeam.plugins.interface.v1.base.Client") as p:
+        yield p
+
+
+@pytest.fixture()
+def read_config():
+    with patch("sunbeam.plugins.ldap.plugin.read_config") as p:
+        yield p
+
+
+@pytest.fixture()
+def update_config():
+    with patch("sunbeam.plugins.ldap.plugin.update_config") as p:
+        yield p
+
+
+@pytest.fixture()
+def ssnap():
+    with patch("sunbeam.clusterd.service.Snap") as p:
+        yield p
+
+
+@pytest.fixture(autouse=True)
+def mock_run_sync(mocker):
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+
+    def run_sync(coro):
+        return loop.run_until_complete(coro)
+
+    mocker.patch("sunbeam.plugins.pro.plugin.run_sync", run_sync)
+    yield
+    loop.close()
+
+
+class FakeLDAPPlugin(LDAPPlugin):
+    def __init__(self):
+        self.config_flags = None
+        self.name = "ldap"
+        self.app_name = self.name.capitalize()
+        self.tf_plan_location = 1
+
+
+class TestAddLDAPDomainStep:
+    def setup_method(self):
+        self.jhelper = AsyncMock()
+        self.tfhelper = Mock(path=Path())
+        self.charm_config = {"domain-name": "dom1"}
+
+    def test_is_skip(self, cclient):
+        self.plugin = FakeLDAPPlugin()
+        step = AddLDAPDomainStep(self.tfhelper, self.jhelper, self.plugin, {})
+        result = step.is_skip()
+        assert result.result_type == ResultType.COMPLETED
+
+    def test_has_prompts(self, cclient):
+        self.plugin = FakeLDAPPlugin()
+        step = AddLDAPDomainStep(self.tfhelper, self.jhelper, self.plugin, {})
+        assert not step.has_prompts()
+
+    def test_enable_first_domain(self, cclient, read_config, update_config, snap):
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {}
+        step = AddLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, self.charm_config
+        )
+        result = step.run()
+        self.tfhelper.write_tfvars.assert_called_with(
+            {
+                "ldap-channel": "2023.1/edge",
+                "enable-ldap": True,
+                "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+            }
+        )
+        self.tfhelper.apply.assert_called_once_with()
+        self.jhelper.wait_until_active.assert_called_once_with(
+            "openstack", ["keystone", "keystone-ldap-dom1"], timeout=900
+        )
+        assert result.result_type == ResultType.COMPLETED
+
+    def test_enable_second_domain(self, cclient, read_config, update_config, snap):
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {
+            "ldap-channel": "2023.1/edge",
+            "enable-ldap": True,
+            "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+        }
+        step = AddLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, {"domain-name": "dom2"}
+        )
+        result = step.run()
+        self.tfhelper.write_tfvars.assert_called_with(
+            {
+                "ldap-channel": "2023.1/edge",
+                "enable-ldap": True,
+                "ldap_apps": {
+                    "dom1": {"domain-name": "dom1"},
+                    "dom2": {"domain-name": "dom2"},
+                },
+            }
+        )
+        self.tfhelper.apply.assert_called_once_with()
+        self.jhelper.wait_until_active.assert_called_once_with(
+            "openstack", ["keystone", "keystone-ldap-dom2"], timeout=900
+        )
+        assert result.result_type == ResultType.COMPLETED
+
+    def test_enable_tf_apply_failed(self, cclient, read_config, update_config, snap):
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {}
+        self.tfhelper.apply.side_effect = TerraformException("apply failed...")
+        step = AddLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, self.charm_config
+        )
+        result = step.run()
+        self.tfhelper.apply.assert_called_once()
+        assert result.result_type == ResultType.FAILED
+        assert result.message == "apply failed..."
+
+    def test_enable_waiting_timed_out(self, cclient, read_config, update_config, snap):
+        self.jhelper.wait_until_active.side_effect = TimeoutException("timed out")
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {}
+        step = AddLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, self.charm_config
+        )
+        result = step.run()
+        self.tfhelper.write_tfvars.assert_called_with(
+            {
+                "ldap-channel": "2023.1/edge",
+                "enable-ldap": True,
+                "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+            }
+        )
+        self.tfhelper.apply.assert_called_once_with()
+        self.jhelper.wait_until_active.assert_called_once_with(
+            "openstack", ["keystone", "keystone-ldap-dom1"], timeout=900
+        )
+        assert result.result_type == ResultType.FAILED
+        assert result.message == "timed out"
+
+
+class TestDisableLDAPDomainStep:
+    def setup_method(self):
+        self.jhelper = AsyncMock()
+        self.tfhelper = Mock(path=Path())
+        self.charm_config = {"domain-name": "dom1"}
+
+    def test_is_skip(self, cclient):
+        self.plugin = FakeLDAPPlugin()
+        step = DisableLDAPDomainStep(self.tfhelper, self.jhelper, self.plugin, "dom1")
+        result = step.is_skip()
+        assert result.result_type == ResultType.COMPLETED
+
+    def test_has_prompts(self, cclient):
+        self.plugin = FakeLDAPPlugin()
+        step = DisableLDAPDomainStep(self.tfhelper, self.jhelper, self.plugin, "dom1")
+        assert not step.has_prompts()
+
+    def test_disable(self, cclient, read_config, update_config, snap):
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {
+            "ldap-channel": "2023.1/edge",
+            "enable-ldap": True,
+            "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+        }
+        step = DisableLDAPDomainStep(self.tfhelper, self.jhelper, self.plugin, "dom1")
+        step.run()
+        self.tfhelper.write_tfvars.assert_called_with(
+            {"ldap-channel": "2023.1/edge", "enable-ldap": True, "ldap_apps": {}}
+        )
+        self.tfhelper.apply.assert_called_once_with()
+
+    def test_disable_tf_apply_failed(self, cclient, read_config, update_config, snap):
+        self.tfhelper.apply.side_effect = TerraformException("apply failed...")
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {
+            "ldap-channel": "2023.1/edge",
+            "enable-ldap": True,
+            "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+        }
+        step = DisableLDAPDomainStep(self.tfhelper, self.jhelper, self.plugin, "dom1")
+        result = step.run()
+        self.tfhelper.write_tfvars.assert_called_with(
+            {"ldap-channel": "2023.1/edge", "enable-ldap": True, "ldap_apps": {}}
+        )
+        self.tfhelper.apply.assert_called_once_with()
+        assert result.result_type == ResultType.FAILED
+        assert result.message == "apply failed..."
+
+    def test_disable_wrong_domain(self, cclient, read_config, update_config, snap):
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {
+            "ldap-channel": "2023.1/edge",
+            "enable-ldap": True,
+            "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+        }
+        step = DisableLDAPDomainStep(self.tfhelper, self.jhelper, self.plugin, "dom2")
+        result = step.run()
+        assert result.result_type == ResultType.FAILED
+        assert result.message == "Domain not found"
+
+
+class TestUpdateLDAPDomainStep:
+    def setup_method(self):
+        self.jhelper = AsyncMock()
+        self.tfhelper = Mock(path=Path())
+        self.charm_config = {"domain-name": "dom1"}
+
+    def test_is_skip(self, cclient):
+        self.plugin = FakeLDAPPlugin()
+        step = UpdateLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, self.charm_config
+        )
+        result = step.is_skip()
+        assert result.result_type == ResultType.COMPLETED
+
+    def test_has_prompts(self, cclient):
+        self.plugin = FakeLDAPPlugin()
+        step = UpdateLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, self.charm_config
+        )
+        assert not step.has_prompts()
+
+    def test_update_domain(self, cclient, read_config, update_config, snap):
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {
+            "ldap-channel": "2023.1/edge",
+            "enable-ldap": True,
+            "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+        }
+        step = UpdateLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, self.charm_config
+        )
+        result = step.run()
+        self.tfhelper.write_tfvars.assert_called_with(
+            {
+                "ldap-channel": "2023.1/edge",
+                "enable-ldap": True,
+                "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+            }
+        )
+        self.tfhelper.apply.assert_called_once_with()
+        self.jhelper.wait_until_active.assert_called_once_with(
+            "openstack", ["keystone", "keystone-ldap-dom1"], timeout=900
+        )
+        assert result.result_type == ResultType.COMPLETED
+
+    def test_update_wrong_domain(self, cclient, read_config, update_config, snap):
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {
+            "ldap-channel": "2023.1/edge",
+            "enable-ldap": True,
+            "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+        }
+        step = UpdateLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, {"domain-name": "dom2"}
+        )
+        result = step.run()
+        assert result.result_type == ResultType.FAILED
+        assert result.message == "Domain not found"
+
+    def test_tf_apply_failed(self, cclient, read_config, update_config, snap):
+        self.tfhelper.apply.side_effect = TerraformException("apply failed...")
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {
+            "ldap-channel": "2023.1/edge",
+            "enable-ldap": True,
+            "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+        }
+        step = UpdateLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, self.charm_config
+        )
+        result = step.run()
+        self.tfhelper.apply.assert_called_once_with()
+        assert result.result_type == ResultType.FAILED
+        assert result.message == "apply failed..."
+
+    def test_update_waiting_timed_out(self, cclient, read_config, update_config, snap):
+        self.jhelper.wait_until_active.side_effect = TimeoutException("timed out")
+        self.tfhelper.apply.side_effect = TerraformException("apply failed...")
+        self.plugin = FakeLDAPPlugin()
+        read_config.return_value = {
+            "ldap-channel": "2023.1/edge",
+            "enable-ldap": True,
+            "ldap_apps": {"dom1": {"domain-name": "dom1"}},
+        }
+        step = UpdateLDAPDomainStep(
+            self.tfhelper, self.jhelper, self.plugin, self.charm_config
+        )
+        result = step.run()
+        self.tfhelper.apply.assert_called_once_with()
+        assert result.result_type == ResultType.FAILED
+        assert result.message == "apply failed..."
